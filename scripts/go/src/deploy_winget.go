8f9c5120574471cee9a2aee379b8d590d8ff6248
Brandon Martinez <brandonam04@outlook.com>
2024-06-25 15:08:42 -0400

    Add scripts that I'll end up using for building this app

---
+++ scripts/go/src/deploy_winget.go
@@ -0,0 +1,4 @@
+// Will probably be the hardest. Has to build msi/exe, then generate manifest, 
+// then make a github release for the installer, then submit the manifest.
+// Will likely end up requiring a docker container or something to use the windows specific tools 
+// that are required here. Or some other solution to that.  
