5d25ba83f61dc518722aa922c65921997f648dfe
Brandon Martinez <brandonam04@outlook.com>
2024-07-10 16:28:28 -0400

    Add the yamlreader and start the schema data file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::io;
 use std::fs;
-use yaml_rust2::Yamlloader;
+use yaml_rust2::YamlLoader;
 
 fn main() {
     println!("Hello, world!");
-    build_fs();
+    build_fs("data/schema.yaml");
 }
-fn build_fs() {// takes a schema as yaml and creates the directories from it
+fn build_fs(file: &str) {// takes a schema as yaml and creates the directories from it
+    let contents: String = fs::read_to_string(file).expect("Could not read file");
+    let schema = YamlLoader::load_from_str(&contents).unwrap();
 
+    println!("Schema loaded!");
     fs::create_dir_all("./test/hi");//use this to make sure whole directory tree is created.
     println!("Called create dir!");
 }
