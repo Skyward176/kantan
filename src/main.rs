19c868dc60ea9395e315d7d1ee848d1accbaaa65
Brandon Martinez <brandonam04@outlook.com>
2024-07-12 16:55:50 -0400

    Create iterator loop for yaml schema
    Now need to break out the directory path logic into a recursive function
    so it can handle subdirectories and even files later.
    
    Then we can have a loop that says for every item in the directories
    field generate paths. Then it recursively generates paths and then
    creates the directories when done. As a result that function is gonna
    need a reference to the base path, which should just be ./ but you never
    know

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use std::io;
-use std::fs;
-use yaml_rust2::YamlLoader;
+use std::fs::File;
+use std::io::prelude::*;
+use fstrings::f;
+use yaml_rust2 as yaml;
 
 fn main() {
     println!("Hello, world!");
     build_fs("data/schema.yaml");
 }
+fn generate_path(path: &str) -> String {
+    let path = f!("{}/{}", "scratch", "test");
+    println!("{}", path);
+}
 fn build_fs(file: &str) {// takes a schema as yaml and creates the directories from it
-    let contents: String = fs::read_to_string(file).expect("Could not read file");
-    let schema = YamlLoader::load_from_str(&contents).unwrap();
+    println!("Trying to open {}", file);
+    let mut contents = File::open(file).unwrap();
+    let mut string = String::new();
+    contents.read_to_string(&mut string).unwrap();
 
-    println!("Schema loaded!");
-    fs::create_dir_all("./test/hi");//use this to make sure whole directory tree is created.
+    let doc = yaml::YamlLoader::load_from_str(&string).unwrap();
+    for item in doc[0]["directories"].clone() {
+        println!("{:?}", item);
+    }
+    
     println!("Called create dir!");
 }
