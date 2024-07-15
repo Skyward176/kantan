e871c114a57e036d8a8ac65a8a9c7bd76e9dd9b0
Brandon Martinez <brandonam04@outlook.com>
2024-07-15 17:00:26 -0400

    Add init command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,60 @@
 use std::fs::File;
 use std::fs::create_dir_all;
-use std::io::prelude::*;
+use std::io;
+use std::io::Read;
 use yaml_rust2 as yaml;
+use clap::{Parser, Subcommand};
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    init {
+        /// lists test values
+        // Optional schema selection
+        #[arg(short, long)]
+        schema: Option<String>,
+        // Optional target selection
+        #[arg(short, long)]
+        target: Option<String>,
+    },
+}
 fn main() {
-    println!("Hello, world!");
-    read_schema("data/schema.yaml", "scratch");
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::init {schema, target}) => {
+            println!("Init command");
+            init()
+        }
+        None => {}
+    }
+
+}
+fn init() {
+    while true {
+        println!("Enter the path to the schema file");
+        let mut file = String::new();
+        io::stdin().read_line(&mut file).expect("Failed to read line");
+        println!("Enter the destination directory");
+        let mut dest = String::new();
+        io::stdin().read_line(&mut dest).expect("Failed to read line");
+        read_schema(file.trim(), dest.trim());
+        println!("Init completed!");
+        break;
+    }
+
 }
 fn generate_paths(base: &str, path: &str) {
     let full_path :String = format!("./{}/{}", base, path);
     create_dir_all(full_path);// need to handle errors here
 }
 fn read_schema(file: &str, dest: &str) {// takes a schema as yaml and creates the directories from it
-    println!("Trying to open {}", file);
     let mut contents = File::open(file).unwrap();
     let mut string = String::new();
     contents.read_to_string(&mut string).unwrap();
